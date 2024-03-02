c4926cd1302e335b0cc998f4bbb135a26a8b45ba
Jack Ren <bjrjk@qq.com>
2024-03-02 06:28:24 +0000

    Implemented Record Layout Dumper Framework & `--filter` option

---
+++ LayoutDumper.cpp
@@ -0,0 +1,123 @@
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace clang;
+
+namespace std {
+template <typename charT>
+inline bool starts_with(const basic_string<charT> &big,
+                        const basic_string<charT> &small) {
+  if (&big == &small)
+    return true;
+  const typename basic_string<charT>::size_type big_size = big.size();
+  const typename basic_string<charT>::size_type small_size = small.size();
+  const bool valid_ = (big_size >= small_size);
+  const bool starts_with_ = (big.compare(0, small_size, small) == 0);
+  return valid_ and starts_with_;
+}
+
+template <typename charT>
+inline bool ends_with(const basic_string<charT> &big,
+                      const basic_string<charT> &small) {
+  if (&big == &small)
+    return true;
+  const typename basic_string<charT>::size_type big_size = big.size();
+  const typename basic_string<charT>::size_type small_size = small.size();
+  const bool valid_ = (big_size >= small_size);
+  const bool ends_with_ =
+      (big.compare(big_size - small_size, small_size, small) == 0);
+  return valid_ and ends_with_;
+}
+} // namespace std
+
+namespace {
+
+class LayoutDumpConsumer : public ASTConsumer {
+  CompilerInstance &Instance;
+  std::map<std::string, std::string> &ParsedArgs;
+
+public:
+  LayoutDumpConsumer(CompilerInstance &Instance,
+                     std::map<std::string, std::string> &ParsedArgs)
+      : Instance(Instance), ParsedArgs(ParsedArgs) {}
+
+  struct Visitor : public RecursiveASTVisitor<Visitor> {
+    std::set<RecordDecl *> RecordDeclSet;
+
+    Visitor() {}
+
+    bool VisitRecordDecl(RecordDecl *RD) {
+      RecordDeclSet.insert(RD);
+      return true;
+    }
+  };
+
+  void HandleTranslationUnit(ASTContext &context) override {
+    Visitor visitor;
+    visitor.TraverseDecl(context.getTranslationUnitDecl());
+
+    std::string filter = ParsedArgs["filter"];
+    for (RecordDecl *RD : visitor.RecordDeclSet) {
+      std::string qualifiedName = RD->getQualifiedNameAsString();
+      if (filter != "" && qualifiedName.find(filter) == std::string::npos)
+        continue;
+      llvm::errs() << "------ Record Decl: " << qualifiedName << "\n";
+      context.DumpRecordLayout(RD, llvm::errs());
+    }
+  }
+};
+
+class LayoutDumpAction : public PluginASTAction {
+  std::map<std::string, std::string> ParsedArgs;
+
+protected:
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 llvm::StringRef) override {
+    return std::make_unique<LayoutDumpConsumer>(CI, ParsedArgs);
+  }
+
+  bool ParseArgs(const CompilerInstance &CI,
+                 const std::vector<std::string> &args) override {
+    ParsedArgs["verbose"] = "false";
+    ParsedArgs["filter"] = "";
+    for (auto &arg : args) {
+      if (arg == "--help") {
+        PrintHelp();
+        return false;
+      } else if (arg == "--verbose") {
+        ParsedArgs["verbose"] = "true";
+      }
+    }
+    for (auto &arg : args) {
+      if (ParsedArgs["verbose"] == "true") {
+        llvm::errs() << "Arg: " << arg << "\n";
+      }
+      if (std::starts_with(arg, std::string("--filter="))) {
+        std::string filter = arg.substr(9);
+        ParsedArgs["filter"] = filter;
+        if (ParsedArgs["verbose"] == "true")
+          llvm::errs() << "ParseArgs: Qualified name filter is " << filter << "\n";
+      }
+    }
+    return true;
+  }
+
+  void PrintHelp() {
+    std::string help = R"EOF(
+clang-object-layout-dumper, A clang plugin dumps C/C++ class or struct's layout.
+Options:
+    --verbose           Dump detailed information when running plugin.
+    --help              Print this help message.
+    --filter=[string]   Only dump the layout of class/struct whose qualified name has [string].
+    )EOF";
+    llvm::errs() << help;
+  }
+};
+
+} // namespace
+
+static FrontendPluginRegistry::Add<LayoutDumpAction>
+    X("layout_dump", "Dump struct/class layout");
---
+++ README.md
@@ -0,0 +1,26 @@
+# clang-object-layout-dumper
+
+A clang plugin dumps C/C++ class or struct's layout.
+
+## Build
+
+```bash
+sudo apt update
+sudo apt install -y build-essential cmake clang gcc ninja-build
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+git clone https://github.com/bjrjk/clang-object-layout-dumper.git clang/tools/clang-object-layout-dumper
+mkdir build
+cd build
+cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=Debug -DLLVM_ENABLE_PROJECTS=clang -DLLVM_TARGETS_TO_BUILD=X86 -DBUILD_SHARED_LIBS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+autoninja
+```
+
+## Run
+
+```bash
+bin/clang++ \
+  -Xclang -load -Xclang lib/LayoutDumper.so -Xclang -plugin -Xclang layout_dump \
+  -fplugin-arg-layout_dump-[ARG1] -fplugin-arg-layout_dump-[ARG2] ... -fplugin-arg-layout_dump-[ARGn] \
+  test.cpp
+```
