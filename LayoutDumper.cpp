5a19880a51f4a251cfd7cd3510cbb15aa590b99c
Jack Ren <bjrjk@qq.com>
2024-03-03 09:08:27 +0000

    Added `--output` and `--concurrent` options

--- LayoutDumper.cpp
+++ LayoutDumper.cpp
@@ -1,3 +1,5 @@
+#include <fstream>
+
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
@@ -38,6 +40,8 @@ namespace {
 class LayoutDumpConsumer : public ASTConsumer {
   CompilerInstance &Instance;
   std::map<std::string, std::string> ParsedArgs;
+  using Layout_t = std::unordered_map<std::string, std::string>;
+  Layout_t Layouts;
 
 public:
   LayoutDumpConsumer(CompilerInstance &Instance,
@@ -63,9 +67,97 @@ public:
             return;
         }
       }
-      llvm::errs() << "------ Record Decl: " << qualifiedName << "\n";
-      context.DumpRecordLayout(RD, llvm::errs());
+      if (!Layouts.count(qualifiedName)) {
+        std::string layoutStr;
+        llvm::raw_string_ostream ss(layoutStr);
+        context.DumpRecordLayout(RD, ss);
+        Layouts.insert({qualifiedName, layoutStr});
+      }
     }
+  }
+
+  ~LayoutDumpConsumer() override {
+    if (ParsedArgs["output"] == "")
+      LayoutOutput(llvm::errs(), Layouts);
+    else
+      HandleFileOutput(ParsedArgs["output"],
+                       ParsedArgs["concurrent"] == "true");
+  }
+
+  template <typename Stream>
+  void QualifiedNameOutput(Stream &os, Layout_t layouts) {
+    for (const auto &[qualifiedName, layout] : layouts) {
+      os << qualifiedName << "\n";
+    }
+  }
+
+  template <typename Stream> void LayoutOutput(Stream &os, Layout_t layouts) {
+    for (const auto &[qualifiedName, layout] : layouts) {
+      os << "------ Record Decl: " << qualifiedName << "\n";
+      os << layout;
+    }
+  }
+
+  void HandleFileOutput(const std::string &path, bool isConcurrent) {
+    std::error_code EC;
+    if (!isConcurrent) {
+      llvm::raw_fd_ostream listStream(path + ".list.log", EC);
+      if (EC)
+        abort();
+      QualifiedNameOutput(listStream, Layouts);
+      llvm::raw_fd_ostream layoutStream(path + ".layout.log", EC);
+      if (EC)
+        abort();
+      LayoutOutput(layoutStream, Layouts);
+      return;
+    }
+    // Concurrent
+
+    // Calculate calculatedLayoutList and sort
+    std::vector<std::string> calculatedLayoutList;
+    for (auto &[qualifiedName, layout] : Layouts) {
+      calculatedLayoutList.push_back(qualifiedName);
+    }
+    std::sort(calculatedLayoutList.begin(), calculatedLayoutList.end());
+
+    // Lock files
+    llvm::raw_fd_ostream lockStream(path + ".lock", EC);
+    if (EC)
+      abort();
+    if (auto L = lockStream.lock()) {
+      std::ifstream listIStream(path + ".list.log");
+      std::ofstream listOStream(path + ".list.log", std::ios::app);
+      std::ofstream layoutStream(path + ".layout.log", std::ios::app);
+      // Get existedLayoutList and sort
+      auto existedLayoutList = getlines(listIStream);
+      std::sort(existedLayoutList.begin(), existedLayoutList.end());
+      // Get difference
+      std::vector<std::string> toBeStoredLayoutList;
+      std::set_difference(
+          calculatedLayoutList.begin(), calculatedLayoutList.end(),
+          existedLayoutList.begin(), existedLayoutList.end(),
+          std::inserter(toBeStoredLayoutList, toBeStoredLayoutList.begin()));
+      // Output
+      for (auto &qualifiedName : toBeStoredLayoutList) {
+        listOStream << qualifiedName << "\n";
+        layoutStream << "------ Record Decl: " << qualifiedName << "\n";
+        layoutStream << Layouts[qualifiedName];
+      }
+    } else
+      abort();
+  }
+
+  static std::vector<std::string> getlines(std::ifstream &fs) {
+    std::vector<std::string> result;
+    fs.seekg(0, std::ios::beg);
+    if (!fs.is_open())
+      return result;
+    while (!fs.eof()) {
+      std::string tmp;
+      std::getline(fs, tmp);
+      result.emplace_back(tmp);
+    }
+    return result;
   }
 };
 
@@ -82,6 +174,8 @@ protected:
                  const std::vector<std::string> &args) override {
     ParsedArgs["verbose"] = "false";
     ParsedArgs["filter"] = "";
+    ParsedArgs["output"] = "";
+    ParsedArgs["concurrent"] = "false";
     for (auto &arg : args) {
       if (arg == "--help") {
         PrintHelp();
@@ -100,6 +194,13 @@ protected:
         if (ParsedArgs["verbose"] == "true")
           llvm::errs() << "ParseArgs: Qualified name filter is " << filter
                        << "\n";
+      } else if (std::starts_with(arg, std::string("--output="))) {
+        std::string path = arg.substr(9);
+        ParsedArgs["output"] = path;
+        if (ParsedArgs["verbose"] == "true")
+          llvm::errs() << "ParseArgs: Output path is " << path << "\n";
+      } else if (arg == "--concurrent") {
+        ParsedArgs["concurrent"] = "true";
       }
     }
     return true;
@@ -112,6 +213,10 @@ Options:
     --verbose           Dump detailed information when running plugin.
     --help              Print this help message.
     --filter=[string]   Only dump the layout of class/struct whose qualified name has [string].
+    --output=[path]     Instead of dumping to stderr, dump the list and layout of class/struct
+                        to [path].list.log and [path].layout.log, respectively.
+    --concurrent        Enable concurrent dumping to the same file by locking the file.
+                        Also deduplicate.
     )EOF";
     llvm::errs() << help;
   }
